720bce29eb3aec30bf8f458ea00e9e27d7033014
abhirajka <abhiraj@gmail.com>
2022-10-27 17:21:38 -0700

    Fixing game

--- raspi_utils/Button.py
+++ raspi_utils/Button.py
@@ -12,7 +12,7 @@ class Button:
         GPIO.setwarnings(False);
 
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=callback, bouncetime=200)
+        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=callback)
 
     def cleanup(self):
         GPIO.cleanup(self.pin);
