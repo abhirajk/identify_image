434e241543f0676f2db43c0c6459cffac2e852bf
abhirajka <abhiraj@gmail.com>
2022-10-28 18:17:17 -0700

    Fixing game

--- raspi_utils/LedButton.py
+++ raspi_utils/LedButton.py
@@ -15,17 +15,17 @@ class LedButton:
         self.led = Led(ledPin);
         self.led.off();
         self.callback = callback;
-        self.button = Button(buttonPin, callback=lambda channel: self.ledButtonCallback(channel, self.led));
+        self.button = Button(buttonPin, callback=self.ledButtonCallback);
 
-    def ledButtonCallback(self, channel, led):
+    def ledButtonCallback(self, channel):
         istate: str = 0;
         if GPIO.input(channel) == GPIO.HIGH:
             istate = 1;
         if GPIO.input(channel) == GPIO.HIGH:
-            led.on();
+            self.led.on();
             self.callback(self, istate);
         else:
-            led.off();
+            self.led.off();
             self.callback(self, istate);
 
     def getLed(self) -> Led:
